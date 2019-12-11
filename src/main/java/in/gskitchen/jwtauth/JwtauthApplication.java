754608a82c43cf26404ea440b586dcf0cea9422b
Sabbir Ahmad <35127031+GSKitchen@users.noreply.github.com>
2019-12-11 17:13:37 +0530

    added comment

--- src/main/java/in/gskitchen/jwtauth/JwtauthApplication.java
+++ src/main/java/in/gskitchen/jwtauth/JwtauthApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JwtauthApplication {
 
+	// main mehtod
 	public static void main(String[] args) {
 		SpringApplication.run(JwtauthApplication.class, args);
 	}
